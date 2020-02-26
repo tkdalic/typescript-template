0187148259565b07da59ee1f2fd79f6c978f35ce
tkdalic <himagami126@gmail.com>
2020-02-26 21:16:31 +0900

    modify: .eslintrc prettierの設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,9 @@ module.exports = {
     },
     "extends": [
         "eslint:recommended",
-        "plugin:@typescript-eslint/eslint-recommended"
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:prettier/recommended",
+        "prettier/@typescript-eslint"
     ],
     "globals": {
         "Atomics": "readonly",
